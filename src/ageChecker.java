14a70e919e7a80372abaec7ee30e3fdea020a88b
Dawid Podsiadly <skill-1@wp.pl>
2022-04-17 20:42:21 +0200

    Update

---
+++ src/ageChecker.java
@@ -0,0 +1,23 @@
+// A simple program that checks if u can buy alcohol.
+
+import java.util.Scanner;
+
+public class ageChecker {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome in our shop!\nEnter your age: ");
+        int age = input.nextInt();
+
+        if (age >= 18) {
+            System.out.println("Thank you for shopping!");
+        }
+        else if (age <= 0) {
+            System.out.println("You have entered wrong value, please try again later.");
+        }
+        else{
+            int years = 18 - age;
+            System.out.println("Sorry, you are too young to buy alcohol.\nSee you in " + years + " years");
+        }
+    }
+}
