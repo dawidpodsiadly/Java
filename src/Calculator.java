0cadcdf62d3385fec1fbd88d45d5c5096c4c670d
Dawid Podsiadly <skill-1@wp.pl>
2022-04-17 17:28:01 +0200

    Update

--- src/Calculator.java
+++ src/Calculator.java
@@ -25,21 +25,26 @@ public class Calculator {
                 case 1:
                     System.out.println("You answer: " + (a + b) + "\n");
                     continue;
+
                 case 2:
                     System.out.println("You answer: " + (a - b) + "\n");
                     break;
+
                 case 3:
                     System.out.println("You answer: " + (a * b) + "\n");
                     break;
+
                 case 4:
                     if(b == 0) {
                         System.out.println("You can't divide by 0!\n");
                         break;
                     }
+
                     else {
                         System.out.println("You answer: " + (a / b) + "\n");
                         break;
                     }
+
                 case 5:
                     System.out.println("Goodbye");
                     break;
